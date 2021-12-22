d6c9a3bf70668ad5347ca70fefd48e2efd2e899a
xiewj <183778760@qq.com>
2021-12-22 18:36:04 +0800

    runtime gmp

---
+++ runtime_source/malloc/malloc.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type smallobj struct {
+	arr [1 << 10]byte
+}
+
+type largeobj struct {
+	arr [1 << 26]byte
+}
+
+func tiny()   {
+	y := 100000
+	fmt.Println(y) // call $runtime.convT64
+}
+
+func large() {
+	large := largeobj{}
+	println(&large)
+	println(unsafe.Sizeof(large)/1024/1024)
+}
+
+func small() {
+	small := smallobj{}
+	print(&small)
+}
+
+func main() {
+	//tiny()
+	//small()
+	large()
+}
